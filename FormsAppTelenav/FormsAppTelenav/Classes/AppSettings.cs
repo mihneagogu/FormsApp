7df07ba883f010c3b9ed7a3bf1c5e2a79e18dd21
mihneagogu <gogumihnea@gmail.com>
2018-02-11 21:52:01 +0200

    Person's worth adjusts by time passed (hardcoded time)

---
+++ FormsAppTelenav/FormsAppTelenav/Classes/AppSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsAppTelenav.Classes
+{
+    public class AppSettings
+    {
+        public AppSettings()
+        {
+
+        }
+
+        public string LastLogin { get; set; }
+        public int CurrentPerson { get; set; }
+    }
+}
--- FormsAppTelenav/FormsAppTelenav/Views/MainView.xaml.cs
+++ FormsAppTelenav/FormsAppTelenav/Views/MainView.xaml.cs
@@ -17,7 +17,7 @@ namespace FormsAppTelenav.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainView : ContentPage
     {
-
+        private string moneyStatement = "";
         Person person = new Person("Mikeymike");
         public Person Person {
             set; get;
@@ -31,6 +31,7 @@ namespace FormsAppTelenav.Views
             person.CurrencyID = 2;
             person.Amount = 5000;
             MeddleWithDB(person);
+            CalculateMoneyToEarn();
 
 
             
@@ -42,7 +43,20 @@ namespace FormsAppTelenav.Views
 
         
 
-
+        private void CalculateMoneyToEarn()
+        {
+            DateTime constantTime = new DateTime(2018, 2, 10, 9, 1, 1, DateTimeKind.Local);
+            DateTime timeNow = DateTime.Now.ToLocalTime();
+            TimeSpan span = timeNow.Subtract(constantTime);
+            double minutes = span.TotalMinutes;
+            double amount = 0;
+            amount += 1 * Math.Floor(minutes / 10);
+            double currentAmount = person.Amount + amount;
+            MoneyStatement = "You have currently " + currentAmount + "$";
+            
+            
+            
+        }
 
         private void ToBank_Clicked(object sender, EventArgs e)
         {
@@ -78,6 +92,17 @@ namespace FormsAppTelenav.Views
             int q = 0;
             await DisplayAlert("ok", ppl.Count.ToString(), "ok");
             
+        }
+
+        private void ToHistory_Clicked(object sender, EventArgs e)
+        {
+            HistoryView historyView = new HistoryView();
+            Navigation.PushAsync(historyView);
+        }
+
+        public string MoneyStatement
+        {
+            set; get;
         }
 
 
