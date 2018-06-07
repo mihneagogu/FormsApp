1c8fb31e857ea2c794d17f1ea4eed15d1132532e
mihneagogu <gogumihnea@gmail.com>
2018-06-07 18:29:20 +0300

    deposits added to credit history view

--- FormsAppTelenav/FormsAppTelenav/Views/CreditsFromMainView.xaml.cs
+++ FormsAppTelenav/FormsAppTelenav/Views/CreditsFromMainView.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using FormsAppTelenav.Classes;
+using System.Collections.ObjectModel;
 
 namespace FormsAppTelenav.Views
 {
@@ -18,14 +20,32 @@ namespace FormsAppTelenav.Views
         {
             NavigationPage.SetHasNavigationBar(this, false);
         }
-
+       
+        private ObservableCollection<Income> bindingList = new ObservableCollection<Income>();
         List<StationaryCredit> credits = new List<StationaryCredit>();
         public List<StationaryCredit> Credits { set { credits = value; } get { return credits; } }
         public CreditsFromMainView(List<StationaryCredit> credits)
         {
             InitializeComponent();
             Credits = credits;
+            DoBindings();
             BindingContext = this;
+        }
+
+        private async void DoBindings()
+        {
+            List<Income> incomes = await App.LocalDataBase.GetIncomes();
+            if (incomes.Count != 0)
+            {
+                foreach (Income i in incomes)
+                {
+                    if (i.Times == -1)
+                    {
+                        bindingList.Add(i);
+                    }
+                }
+                depositList.ItemsSource = bindingList;
+            }
         }
 
 
